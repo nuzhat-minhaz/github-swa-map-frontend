329a6403ab3712b41475f4aed1047c284abb8542
Anthony Chu <anthony@anthonychu.ca>
2022-07-07 10:30:08 -0700

    Use login

--- api/CreateOrUpdatePerson/index.js
+++ api/CreateOrUpdatePerson/index.js
@@ -1,5 +1,15 @@
 module.exports = async function (context, req) {
+    const user = req.user;
+    
+    if (!user && user.username) {
+        context.res = {
+            status: 401,
+        }
+        return;
+    }
+
     const person = req.body;
+    person.id = user.username;
 
     context.bindings.person = person;
 
